62240d5ae7e51c2b940336b8b62589b3402e4f8e
Meriem <meri.mdjil09@gmail.com>
2023-09-28 06:41:46 -0700

    100-is_palindrome.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -5,6 +5,8 @@ int actual_sqrt_recursion(int n, int i);
 /**
  * _sqrt_recursion - returns the natural square root of a number
  * @n: number to calculate the square root
+ *
+ * Return: the resulting square root
  */
 int _sqrt_recursion(int n)
 {
